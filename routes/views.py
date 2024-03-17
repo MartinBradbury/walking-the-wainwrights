b4d75394dc08d3d670aa38bdc1646d35e6af3b55
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-17 23:31:16 +0000

    created a http response in view to test deployment in heroku

--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def routes(request):
-    return render(request, 'routes.html')
+def routes (request):
+    return HttpResponse("Testing")
+
+
+
+# def routes(request):
+#     return render(request, 'routes.html')

25cd212aa14de138f72eed4020d921040decea15
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-18 20:49:27 +0000

    updated view to only show posts that have been aperoved and published.

--- routes/views.py
+++ routes/views.py
@@ -3,13 +3,11 @@ from django.shortcuts import render
 from .models import Route
 
 def routes(request):
-    route_list = Route.objects.all()
+    route_list = Route.objects.filter(status=1) # Filter routes with status 1
     paginator = Paginator(route_list, 4)
     page_number = request.GET.get('page')
     page_object = paginator.get_page(page_number)
     return render(request, 'routes/routes.html', {'page_object': page_object})
-
-
 
 
 def routes_detail(request, slug):

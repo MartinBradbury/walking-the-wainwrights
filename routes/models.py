95c5a5bd5b4f7a109c8baace3006b68212a5c724
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-18 08:33:48 +0000

    migrated post routes model

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
+
+class Routes(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    content = models.TextField()
+    os_url = models.CharField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post_edit = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    flagged = models.BooleanField()
+
+    def __str__(self):
+        return self.title

9fa11cbed4f04cb5cc19637e60d26946fe2c2a3d
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-18 09:48:09 +0000

    rolled back migrations to add author to database migrations, displayed in html.

--- routes/models.py
+++ routes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
@@ -6,6 +7,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Route(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="route_post"
+    )
     content = models.TextField()
     os_url = models.CharField()
     created_on = models.DateTimeField(auto_now_add=True)

9dc6968ca0f1d27698e9c699f9c5cb6fc9cfa9f8
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-18 20:43:40 +0000

    moved the routes and route_detail into a new folder routes in the templates in the app. Updated view links

--- routes/views.py
+++ routes/views.py
@@ -5,10 +5,9 @@ from .models import Route
 def routes(request):
     route_list = Route.objects.all()
     paginator = Paginator(route_list, 4)
-
     page_number = request.GET.get('page')
     page_object = paginator.get_page(page_number)
-    return render(request, 'routes.html', {'page_object': page_object})
+    return render(request, 'routes/routes.html', {'page_object': page_object})
 
 
 
@@ -16,4 +15,4 @@ def routes(request):
 def routes_detail(request, slug):
     # return HttpResponse(slug)
     route = Route.objects.get(slug=slug)
-    return render(request, 'routes_detail.html', {'route':route})
+    return render(request, 'routes/routes_detail.html', {'route':route})

a911e37ad88f20b8c93c6b17eca3da238b46c5a2
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-18 21:36:33 +0000

    installed whitenoise and collected static files. created runtime.txt

--- walking_wainwrights/settings.py
+++ walking_wainwrights/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-martinbradb-walkingthew-k90yxxjn113.ws-eu110.gitpod.io', '.herokuapp.com']
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,6 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'walking_wainwrights/static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'walking_wainwrights/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

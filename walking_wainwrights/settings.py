2b56940d92fbf74bb10fbe89f56186a8f854f43f
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-18 00:07:14 +0000

    connected elephant db

--- walking_wainwrights/settings.py
+++ walking_wainwrights/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(@f-y-k8x5f(s=e_10c=x)z$jrpa%xn@b#e=*x_c^w8q=(nz5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-martinbradb-walkingthew-k90yxxjn113.ws-eu110.gitpod.io', '.herokuapp.com']
 
@@ -75,11 +78,15 @@ WSGI_APPLICATION = 'walking_wainwrights.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 

3b7d5ee76765e8435bd99b35f1d5c7cb4bade9c4
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-23 18:39:07 +0000

    created gallery database and migrated

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Gallery
 
-# Register your models here.
+@admin.register(Gallery)
+class GalleryAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'author', 'status','flagged',)
+    search_fields = ['title', 'author', 'flagged', 'status',]
+    list_filter = ('status', 'flagged',)
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Gallery(models.Model):
+    user_img = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=255, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+    flagged = models.BooleanField()
+
+    def __str__(self):
+        return self.title

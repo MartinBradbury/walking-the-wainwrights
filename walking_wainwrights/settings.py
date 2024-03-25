d56a666c9f57d000bd5b9934f9ac53a4677efadc
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-25 12:30:01 +0000

    debug True and had to reload wokspace so updated allowed_hosts and CSRF trusted

--- walking_wainwrights/settings.py
+++ walking_wainwrights/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-martinbradb-walkingthew-k90yxxjn113.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-martinbradb-walkingthew-ovuwxqd84fc.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -115,7 +115,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://8000-martinbradb-walkingthew-k90yxxjn113.ws-eu110.gitpod.io",
+    "https://8000-martinbradb-walkingthew-ovuwxqd84fc.ws-eu110.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
